4b3b8fd845543277fede6efaac9b9600d1b91f31
kisses-k <kamilahvelante@gmail.com>
2024-07-07 01:28:22 +0800

    added db in the dashboard
    count is not final

--- src/admin/pages/home.jsx
+++ src/admin/pages/home.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 // Styles
 import "../../styles.css";
@@ -9,6 +10,37 @@ import PruLogo from "../../assets/pru-logo-main.svg";
 import SideNav from "../components/sidenav";
 
 function Home() {
+  const [applicant, setApplicant] = useState([]);
+  const [beneficiary, setBeneficiary] = useState([]);
+  const [employer, setEmployer] = useState([]);
+  const [applicantTotal, setApplicantTotal] = useState(4);
+  const [beneficiaryTotal, setBeneficiaryTotal] = useState(4);
+  const [employerTotal, setEmployerTotal] = useState(4);
+  const[search, setSearch] = useState('');
+  console.log(search)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5020/applicants")
+      .then((response) => {
+        setApplicant(response.data);
+      })
+      axios
+      .get("http://localhost:5020/beneficiaries")
+      .then((response) => {
+        setBeneficiary(response.data);
+      })
+      axios
+      .get("http://localhost:5020/employers")
+      .then((response) => {
+        setEmployer(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  });
+  
   return (
     <>
       <div>
@@ -31,7 +63,8 @@ function Home() {
                   <input
                     className="search"
                     type="text"
-                    placeholder="Search Employee, Beneficiary, Employee"
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search Employee, Beneficiary, Employer"
                   />
                 </div>
               </div>
@@ -43,19 +76,19 @@ function Home() {
                 <div className="dashboard-container">
                   <div className="num-container" id="numContainer1">
                     <div className="memberNum total-container">
-                      <h2>40</h2>
+                      <h2>{applicantTotal}</h2>
                       <p>Total Number of Members</p>
                     </div>
                   </div>
                   <div className="num-container" id="numContainer2">
                     <div className="beneficiaryNum total-container">
-                      <h2>40</h2>
+                      <h2>{beneficiaryTotal}</h2>
                       <p>Total Number of Beneficiaries</p>
                     </div>
                   </div>
                   <div className="num-container" id="numContainer3">
                     <div className="employernum total-container">
-                      <h2>40</h2>
+                      <h2>{employerTotal}</h2>
                       <p>Total Number of Employers</p>
                     </div>
                   </div>
@@ -64,16 +97,98 @@ function Home() {
             </div>
             <div className="recent-container">
               <div className="recent-rectangle">
-                <h3 className="recent-text">Recent Members</h3>
-                <hr />
+              <Link to="/dbMembers"> 
+                <h3 className="recent-text"> Members 
+                  <i class='bx bxs-chevron-right'></i></h3> 
+              </Link>
+                
+                <table className="table dashboard-members">
+                  <thead>
+                    <tr>
+                      <th>ApplicantID</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {applicant.filter((a) => {
+                      return search.toLowerCase() === '' ? a
+                        : a.ApplicantName.toLowerCase().includes(search.toLowerCase());
+                    })
+                      .map((a, key) => (
+                        <tr key={key}>
+                          <td className="data-container">
+                            {a.ApplicantID}
+                          </td>
+                          <td className="data-container">
+                            {a.ApplicantName}
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
               </div>
               <div className="recent-rectangle">
-                <h3 className="recent-text">Recent Beneficiaries</h3>
-                <hr />
+              <Link to="/dbBeneficiaries"> 
+              <h3 className="recent-text"> Beneficiaries 
+                <i class='bx bxs-chevron-right'></i></h3> 
+              </Link>
+                <table className="table dashboard-beneficiaries">
+                  <thead>
+                    <tr>
+                      <th>ApplicantID</th>
+                      <th>BeneficiaryCode</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {beneficiary.filter((b) => {
+                      return search.toLowerCase() === '' ? b
+                        : b.BeneficiaryName.toLowerCase().includes(search.toLowerCase());
+                    })
+                      .map((b, key) => (
+                        <tr key={key}>
+                          <td className="data-container">
+                            {b.ApplicantID}
+                          </td>
+                          <td className="data-container">
+                            {b.BeneficiaryCode}
+                          </td>
+                          <td className="data-container">
+                            {b.BeneficiaryName}
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
               </div>
               <div className="recent-rectangle">
-                <h3 className="recent-text">Top Employers</h3>
-                <hr />
+              <Link to="/dbEmployer"> <h3 className="recent-text"> Employers 
+                <i class='bx bxs-chevron-right'></i></h3> 
+              </Link>
+                <table className="table dashboard-beneficiaries">
+                  <thead>
+                    <tr>
+                      <th>EmployerCode</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {employer.filter((e) => {
+                      return search.toLowerCase() === '' ? e
+                        : e.EmpOrBusName.toLowerCase().includes(search.toLowerCase());
+                    })
+                      .map((e, key) => (
+                        <tr key={key}>
+                          <td className="data-container">
+                            {e.EmployerCode}
+                          </td>
+                          <td className="data-container">
+                            {e.EmpOrBusName}
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
